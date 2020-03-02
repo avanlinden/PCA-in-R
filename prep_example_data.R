d4ec61fb9b030e10fb8a47e9f97765b5bef2238d
avanlinden <11965371+avanlinden@users.noreply.github.com>
2020-03-02 11:12:39 -0500

    import data and previous code files

---
+++ prep_example_data.R
@@ -0,0 +1,34 @@
+#Load libraries
+
+library(tidyverse)
+
+# load data as a dplyr tibble
+
+vertData <- as_tibble(read.csv("ruminant_vert_morph_data.csv"))
+vertData
+
+abrev <- c("CH", "CL", "CW")
+fullname <- c("centrumHeight", "centrumLength", "centrumWidth")
+
+vertData <- vertData %>%
+  select(-X,-type, logBMSS) %>%  #remove columns we don't need
+  filter(!is.na(value)) %>%  #remove NAs -- not necessary, prcomp will handle it, but nice to do
+  filter(vertebra == 'C2') %>% #look at just the second cervical vertebra (C2)
+  mutate(measureName = str_replace_all( #add column with full names of measurements for non-morphologists
+    measure,
+    c("CH" = "centrumHeight", 
+      "CL" = "centrumLength", 
+      "CW" = "centrumWidth",
+      "DL" = "densLength",
+      "DW" = "densWidth",
+      "NSL" = "neuralSpineLength",
+      "NSLA" = "neuralSpineLeverArm",
+      "preZD" = "preZygDist",
+      "TPLA" = "transProcessLeverArm"
+      )
+  )) %>%
+  select(catNum, vertebra, measure, measureName, value, logValue, sex, spp, logBodyMass = logBMSS, fightStyle) #select columns in different order, rename body mass column
+
+vertData #check tibble
+
+
